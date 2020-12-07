310c0013ba65d41739e8c2507deed63d4978591f
ErickJMenezes <erick123j@hotmail.com>
2020-12-07 09:20:55 -0300

    Refactory

--- tests/Clients/InterfaceWithoutApiAttribute.php
+++ tests/Clients/InterfaceWithoutApiAttribute.php
@@ -1,9 +1,6 @@
 <?php
 
-
 namespace Tests\Clients;
-
-
 
 interface InterfaceWithoutApiAttribute
 {
--- tests/config/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require dirname(__DIR__) . '/../vendor/autoload.php';
-$classLoader = new \Composer\Autoload\ClassLoader();
-$classLoader->addPsr4("Tests\\", dirname(__DIR__), true);
-$classLoader->register();
